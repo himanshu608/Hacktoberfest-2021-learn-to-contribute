48bcf55b69d1029e0032c68a8adcf2492931d686
Ashish Jangir <61355945+ashishjangir02082001@users.noreply.github.com>
2021-10-01 21:05:32 +0530

    improvised the code
    improvised the code

--- subtract.py
+++ subtract.py
@@ -8,6 +8,6 @@ num2 = input('Enter second number: ')
 
 sum = float(num1) - float(num2)
 
-# Display the sum
+# Display the sum to the user
 
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
