cb5c6c9a196416078de9a5dd607ce08ae7096155
Khushi-agarwal <khushiagarwal226@gmail.com>
2021-10-02 19:17:11 +0530

    Atm program added

---
+++ Atm.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class atm
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    double a=sc.nextDouble();
+	    double am=sc.nextDouble();
+	    if(a%5!=0||(a+0.5)>am)
+	    System.out.println(am);
+	    else
+	   { double d=(am-a-0.5);
+	    System.out.println(d);
+	
+	}
+}
+}
