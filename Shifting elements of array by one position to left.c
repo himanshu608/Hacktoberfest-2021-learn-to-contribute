365f632c669e7269980630ad76fc035e7efba822
Chayan Debbarma <chayan.nit24@gmail.com>
2021-10-02 18:44:16 +0530

    Create Shifting elements of array by one position to left.c

---
+++ Shifting elements of array by one position to left.c
@@ -0,0 +1,44 @@
+// Program to shift elements of the array by one position to the left without reversing the array.
+
+#include<stdio.h>
+void main()
+{
+	int a[100];
+	int i,n;
+	int temp;
+	printf("Enter number of elements to be inserted : \n");
+	scanf("%d",&n);
+	printf("Enter elements in the array : \n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	temp=a[0];
+	for (i=0;i<n-1;i++)
+	{
+    	a[i]=a[i+1];
+	}
+	a[n-1]=temp;
+    
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
+}
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*
+
+Output :- 
+
+Enter number of elements to be inserted :
+5
+Enter elements in the array :
+7
+9
+5
+1
+3
+9 5 1 3 7
+
+*/
