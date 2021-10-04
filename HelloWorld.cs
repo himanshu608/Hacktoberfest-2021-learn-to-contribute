d5c0a227aba83c1aef910474af6889342a4c1ed3
Pabodha Wimalasuriya <bhashiwimalasuriya@gmail.com>
2021-10-04 10:19:44 +0530

    Create HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,10 @@
+// Hello World! program in C#
+namespace HelloWorld
+{
+    public class Program {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
