8526a47d20ec5c590ff5015764a8effa0569dcf8
Vinay Basnal <76448563+vinay243@users.noreply.github.com>
2021-10-25 22:48:23 +0530

    Update Nth prime number.cpp

--- Nth prime number.cpp
+++ Nth prime number.cpp
@@ -6,9 +6,7 @@ void prime(int n)
 {
     int count=1;
     if(n==1)
-    {
         cout<<"The 1st prime element is = "<<2;
-    }
 for(int j=3;j<1000;j++)
 {
     int flag=0;
@@ -22,9 +20,7 @@ for(int j=3;j<1000;j++)
      }
  }
  if(flag==0)
- {
   count++;   
- }
  if(count==n)
  {
   cout<<"The "<<n<<"th prime element is = "<<j<<endl;  
