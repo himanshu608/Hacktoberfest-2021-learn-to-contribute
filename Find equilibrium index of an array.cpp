550b674c2e13c02ea8d55998a5a7ec7e6d3c618e
yashym3171 <93377707+yashym3171@users.noreply.github.com>
2021-10-30 22:39:55 +0530

    update c program to find equilibrium index

--- Find equilibrium index of an array.cpp
+++ Find equilibrium index of an array.cpp
@@ -1,40 +1,44 @@
+// C program to find equilibrium
+// index of an array
 
-#include <iostream>
-#include <numeric>
-using namespace std;
- 
-// Optimized method to find the equilibrium index of an array
-void findEquilibriumIndex(int A[], int n)
+#include <stdio.h>
+
+int equilibrium(int arr[], int n)
 {
-    // `total` stores the sum of all the array elements
-    int total = accumulate(A, A + n, 0);
- 
-    // `right` stores the sum of elements of subarray `A[i+1…n)`
-    int right = 0;
- 
-    // traverse the array from right to left
-    for (int i = n - 1; i >= 0; i--)
-    {
-        /* `i` is an equilibrium index if the sum of elements of subarray `A[0…i-1]`
-           is equal to the sum of elements of the subarray `A[i+1…n)` i.e.
-           `(A[0] + A[1] + … + A[i-1])` = `(A[i+1] + A[i+2] + … + A[n-1])` */
- 
-        // sum of elements of the left subarray `A[0…i-1]` is `total - (A[i] + right)`
-        if (right == total - (A[i] + right)) {
-            cout << "Equilibrium Index found " << i << endl;
-        }
- 
-        // new right is `A[i] + (A[i+1] + A[i+2] + … + A[n-1])`
-        right += A[i];
-    }
+	int i, j;
+	int leftsum, rightsum;
+
+	/* Check for indexes one by one until
+	an equilibrium index is found */
+	for (i = 0; i < n; ++i) {	
+
+		/* get left sum */
+		leftsum = 0;
+		for (j = 0; j < i; j++)
+			leftsum += arr[j];
+
+		/* get right sum */
+		rightsum = 0;
+		for (j = i + 1; j < n; j++)
+			rightsum += arr[j];
+
+		/* if leftsum and rightsum are same,
+		then we are done */
+		if (leftsum == rightsum)
+			return i;
+	}
+
+	/* return -1 if no equilibrium index is found */
+	return -1;
 }
- 
+
+// Driver code
 int main()
 {
-    int A[] = { 0, -3, 5, -4, -2, 3, 1, 0 };
-    int n = sizeof(A) / sizeof(A[0]);
- 
-    findEquilibriumIndex(A, n);
- 
-    return 0;
+	int arr[] = { -7, 1, 5, 2, -4, 3, 0 };
+	int arr_size = sizeof(arr) / sizeof(arr[0]);
+	printf("%d", equilibrium(arr, arr_size));
+
+	getchar();
+	return 0;
 }
