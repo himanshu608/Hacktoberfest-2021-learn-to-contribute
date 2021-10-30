f1942d7bb9dfadb03f2e1e3a096259319a8cfdc7
ashishk9999 <93444784+ashishk9999@users.noreply.github.com>
2021-10-31 01:12:20 +0530

    Delete Swap Two Variables directory

--- Swap Two Variables/using temporary variable.py
+++
@@ -1,9 +0,0 @@
-a = 99
-b = 1
-
-temp = a
-a = b
-b = temp
-
-print('The value of a is: {}'.format(a))
-print('The value of b is: {}'.format(b))
--- Swap Two Variables/without temporary variable.py
+++
@@ -1,6 +0,0 @@
-a = 99
-b = 1
-
-a, b = b, a
-print("The value of a =", a)
-print("The value of b =", b)
