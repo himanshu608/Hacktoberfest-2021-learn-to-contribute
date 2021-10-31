9952b0aebae924aa457d060ee4d9c9b56be553bb
SonuKr23 <93459987+SonuKr23@users.noreply.github.com>
2021-10-31 12:59:35 +0530

    ASCII value.cpp

---
+++ ASCII value.cpp
@@ -0,0 +1,29 @@
+// Program to print ASCII value of given Character.
+
+#include <iostream.h>
+
+#include <conio.h>
+
+void main()
+
+{
+
+	clrscr();	char ch;
+
+	cout << " Enter any character ::: ";
+
+	cin >> ch;
+
+	cout << "\n The ASCII value of \'" << ch << "\' is ::: " << int(ch);
+
+	getch();
+
+}
+
+ Output:
+
+ 
+
+ Enter any character ::: A
+
+ The ASCII value of 'A' is ::: 65
