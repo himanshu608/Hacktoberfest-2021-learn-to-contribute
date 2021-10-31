1e8b9cc97450fda5455f7694b4e2af5b8af540c4
ashishk9999 <93444784+ashishk9999@users.noreply.github.com>
2021-10-31 21:33:34 +0530

    .

--- password.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-a=int(input("Enter length of Password:"))
-
-char = string.ascii_letters + string.digits + string.punctuation
-passw = ''.join(random.choice(char) for i in range(a))
-
-print("The Password generated randomly is:", passw)
